1a31e18d1b998a3ca2415b62b7cc2e930ba47bb8
Kate K <username@nope.local>
2023-07-15 22:22:11 +0300

    Some new changes to .tf

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 The task is to deploy Sonarqube application Helm chart to Minikube. It was implemented on Ubuntu 20 machine.
 
+Elasticsearch needs seccomp filter available, to check if it's vailable on your kernel, run:
+grep SECCOMP /boot/config-$(uname -r)
+
+If it's there, you'll see:
+CONFIG_HAVE_ARCH_SECCOMP_FILTER=y
+CONFIG_SECCOMP_FILTER=y
+CONFIG_SECCOMP=y
+
 Clone the repo and cd into it (you need Git to be installed on your machine, on Ubuntu 20 it's there already)
 
 To run stuff you will need installed:
 
-1. Docker engine
+1. Docker engine (you can use VirtualBox instead, but depending on how you run your machine, you can have issues with starting Minikube on VirtualBox)
 2. Minikube
 3. Kubectl
 5. Helm
--- allinall.tf
+++ allinall.tf
@@ -39,6 +39,26 @@ resource "helm_release" "sonarqube" {
     value = "10.1.0"
   }
   set {
+    name  = "securityContext.privileged"
+    value = "true"
+  }
+  set {
+    name  = "securityContext.allowPrivilegeEscalation"
+    value = "true"
+  }
+  set {
+    name  = "nginx.enabled"
+    value = "false"
+  }
+  set {
+    name  = "initSysctl.enabled"
+    value = "false"
+  }
+  set {
+    name  = "initFs.enabled"
+    value = "false"
+  }
+  set {
     name  = "postgresql.enabled"
     value = "true"
   }
@@ -68,31 +88,30 @@ resource "helm_release" "sonarqube" {
     }
 }
 
-resource "helm_release" "sonarDB" {
-  name = "postgresql"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart = "postgresql"
-
-  set {
-    name  = "postgresqlPostgresPassword"
-    value = "postgresRootPass"
-  }
-
-  set {
-    name  = "postgresqlUsername"
-    value = "sonarUser"
-  }
-  set {
-    name  = "postgresqlPassword"
-    value = "sonarPass"
-  }
-  set {
-    name  = "postgresqlDatabase"
-    value = "sonarDB"
-  }
-  set {
-    name = "service.port"
-    value = "5432"
-  }
-}
-
+# resource "helm_release" "sonarDB" {
+#   name = "postgresql"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart = "postgresql"
+#
+#   set {
+#     name  = "postgresqlPostgresPassword"
+#     value = "postgresRootPass"
+#   }
+#
+#   set {
+#     name  = "postgresqlUsername"
+#     value = "sonarUser"
+#   }
+#   set {
+#     name  = "postgresqlPassword"
+#     value = "sonarPass"
+#   }
+#   set {
+#     name  = "postgresqlDatabase"
+#     value = "sonarDB"
+#   }
+#   set {
+#     name = "service.port"
+#     value = "5432"
+#   }
+# }
