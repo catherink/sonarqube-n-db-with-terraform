a590b7ad62026c6f7cacc8ea3eb59e6d921e811b
Kate K <username@nope.local>
2023-08-02 00:45:06 +0300

    Yes, it's that complicated

--- risky.sh
+++ risky.sh
@@ -13,29 +13,29 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
 
 sudo usermod -aG docker $USER && newgrp docker
 
 # Installing minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb > /dev/null
+sudo dpkg -i minikube_latest_amd64.deb > /dev/null
 
 # To install Kubectl from Minikube we should start Minikube first
 # Setting Docker as default driver here and enabling ingress
-minikube config set driver docker
-minikube start
-minikube addons enable ingress
+minikube config set driver docker > /dev/null
+minikube start > /dev/null
+minikube addons enable ingress > /dev/null
 
 # After installing and starting Minikube this will install kubectl
-minikube kubectl -- get po -A
+minikube kubectl -- get po -A > /dev/null
 
 # Installing Helm
-wget https://get.helm.sh/helm-v3.12.2-linux-amd64.tar.gz
-tar zxvf helm-v3.12.2-linux-amd64.tar.gz
+wget https://get.helm.sh/helm-v3.12.2-linux-amd64.tar.gz > /dev/null
+tar zxvf helm-v3.12.2-linux-amd64.tar.gz > /dev/null
 sudo mv linux-amd64/helm /usr/bin/helm
 
 # Installing Terraform
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install -y terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+sudo apt update && sudo apt install -y terraform > /dev/null
