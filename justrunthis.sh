8d6415eeb3a39532922a3a73d94ad2c5f4408d04
Kate K <username@nope.local>
2023-08-02 00:14:12 +0300

    Ok, few fixes

--- justrunthis.sh
+++ justrunthis.sh
@@ -3,11 +3,13 @@
 # Without this Elasticsearch (which is part of Sonarqube) will not run, but it was like that on CentOS
 # sudo sysctl -w vm.max_map_count=262144
 
+# UNCOMMENT MINIKUBE COMMANDS IN THIS SECTION IF YOU WON'T RUN risky.sh
+# THIS COULD BE A CASE IF YOU HAVE ALL THE TOOLS MENTIONED IN README ALREADY INSTALLED
 # Setting docker as default driver for minikube
-minikube config set driver docker
+# minikube config set driver docker
 # Starting minikube and enabling Nginx ingress
-minikube start
-minikube addons enable ingress
+# minikube start
+# minikube addons enable ingress
 
 # Deploying all the stuff
 terraform init
@@ -15,5 +17,6 @@ terraform apply -auto-approve
 
 # Configuring ingress
 minikube kubectl -- apply -f ingress.yaml
+# Look how cool is that
 minikubeip=$(minikube ip)
 echo "Sonarqube URL: https://$minikubeip/sonarqube/"
--- risky.sh
+++ risky.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-sudo apt update
-
 # Installing Docker engine
-sudo apt install -y ca-certificates curl gnupg
+sudo apt update && sudo apt install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -15,7 +13,7 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo usermod -aG docker $USER && newgrp docker
 
@@ -23,8 +21,14 @@ sudo usermod -aG docker $USER && newgrp docker
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 
-# After installing Minikube this will install kubectl
-minikube kubectl -- get po -A > /dev/null
+# To install Kubectl from Minikube we should start Minikube first
+# Setting Docker as default driver here and enabling ingress
+minikube config set driver docker
+minikube start
+minikube addons enable ingress
+
+# After installing and starting Minikube this will install kubectl
+minikube kubectl -- get po -A
 
 # Installing Helm
 wget https://get.helm.sh/helm-v3.12.2-linux-amd64.tar.gz
@@ -34,4 +38,4 @@ sudo mv linux-amd64/helm /usr/bin/helm
 # Installing Terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt install terraform
+sudo apt update && sudo apt install terraform
